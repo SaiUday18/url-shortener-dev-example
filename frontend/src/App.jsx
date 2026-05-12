cb59e8ed2e3d7e51378570eee9ea6b8eb4bce261
@Uday7-Sai <2310030045@klh.edu.in>
2026-05-12 13:50:58 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import LoginPage from './Pages/LoginPage/LoginPage';
+import './index.css';
+import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
+import { HeaderMegaMenu } from './Components/Navbar/HeaderMegaMenu';
+import Profile from './Components/Profile/Profile';
+
+function App() {
+  return (
+    <Router>
+      <HeaderMegaMenu />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route element={<PrivateRoute />}>
+
+
+        </Route>
+      </Routes>
+    </Router>
+  )
+}
+
+
+export default App
